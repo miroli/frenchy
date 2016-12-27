2f350671504ee41270db7c9ef108f9d52cae4ffd
miroli <robin.linderborg@gmail.com>
2016-12-27 20:20:54 +0100

    explicit requires

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from distutils.core import setup
+
+requires = [
+    'beautifulsoup4',
+    'bs4',
+    'html5lib',
+    'lxml',
+    'numpy',
+    'pandas',
+    'python-dateutil',
+    'pytz',
+    'six',
+    'webencodings'
+]
+
 setup(
-    name = 'frenchy',
-    packages = ['frenchy'],
-    version = '0.2',
-    description = 'A pandas friendly french election scraper',
-    author = 'Robin Linderborg',
-    author_email = 'robin.linderborg@gmail.com',
-    url = 'https://github.com/miroli/frenchy',
-    download_url = 'https://github.com/miroli/frenchy/tarball/0.2',
-    keywords = ['france', 'scraper', 'elections'],
+    name='frenchy',
+    packages=['frenchy'],
+    version='0.3',
+    description='A pandas friendly french election scraper',
+    author='Robin Linderborg',
+    author_email='robin.linderborg@gmail.com',
+    url='https://github.com/miroli/frenchy',
+    download_url='https://github.com/miroli/frenchy/tarball/0.3',
+    install_requires=requires,
+    keywords=['france', 'scraper', 'elections'],
 )
