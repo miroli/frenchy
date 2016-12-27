fcb8f33c1295ccac3a79103e3fc92fa1b354c4e2
miroli <robin.linderborg@gmail.com>
2016-12-27 20:02:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 setup(
     name = 'frenchy',
     packages = ['frenchy'],
-    version = '0.1',
+    version = '0.2',
     description = 'A pandas friendly french election scraper',
     author = 'Robin Linderborg',
     author_email = 'robin.linderborg@gmail.com',
     url = 'https://github.com/miroli/frenchy',
-    download_url = 'https://github.com/miroli/frenchy/tarball/0.1',
+    download_url = 'https://github.com/miroli/frenchy/tarball/0.2',
     keywords = ['france', 'scraper', 'elections'],
 )
